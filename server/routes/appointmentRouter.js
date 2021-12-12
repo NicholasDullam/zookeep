e252295f9b38afe877a34395f2caef0db38aaced
Nicholas Dullam <ndullam@purdue.edu>
2021-12-11 19:57:16 -0500

    fdsaflsdj

---
+++ server/routes/appointmentRouter.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const AppointmentController = require('../controllers/appointmentController')
+
+const router = express.Router()
+
+router.post('/appointments', AppointmentController.createAppointment)
+router.get('/appointments', AppointmentController.getAppointments)
+router.put('/appointments/:_id', AppointmentController.updateAppointmentById)
+router.delete('/appointments/:_id', AppointmentController.deleteAppointmentById)
+
+module.exports = router;
